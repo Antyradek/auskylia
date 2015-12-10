a0b8fea729176fadeaa58fe0f4c64a964d702ea0
Antyradek <antyradek@gmail.com>
2015-12-10 05:59:35 +0100

    Bazowa strona

--- application/view/js/main.js
+++ application/view/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    //zaczynamy
+    //TODO polecenie AJAX i ściągnięcie listy miast, rozpakowanie XML i aktualizacja pól HTML. Puki co, twardo wpisana tablica:
+    var cities = ["Gdańsk", "Warszawa", "Kraków", "Radom", "Poznań", "Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide"];
+    updateCitySelections(cities);
+
 });
+
+///aktualiazcja wyboru miast
+function updateCitySelections(cities)
+{
+    for(var i = 0; i < cities.length; i++)
+    {
+        var text = "<option>" + cities[i] + "</option>";
+        $(".citySelect").append(text);
+    }
+}
