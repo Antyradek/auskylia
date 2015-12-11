a17fdb21285153a42cac555e5e27f4d1a4add698
Antyradek <antyradek@gmail.com>
2015-12-11 21:42:15 +0100

    Klepsydraaaa

--- application/view/js/main.js
+++ application/view/js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
-    //TODO polecenie AJAX i ściągnięcie listy miast, rozpakowanie XML i aktualizacja pól HTML. Puki co, twardo wpisana tablica:
+    setHourglass(true);
+    $.post("controller", {command : "get_cities"}, function(data)
+    {
+        alert("Odebrano: " + data);
+    });
     var cities = ["Gdańsk", "Warszawa", "Kraków", "Radom", "Poznań", "Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide"];
     updateCitySelections(cities);
+    setHourglass(false);
 
+    $("#mainForm").submit(function(event)
+    {
+        event.preventDefault();
+        setHourglass(true);
+    });
 });
 
 ///aktualiazcja wyboru miast
@@ -12,5 +22,17 @@ function updateCitySelections(cities)
     {
         var text = "<option>" + cities[i] + "</option>";
         $(".citySelect").append(text);
+    }
+}
+
+function setHourglass(setOn)
+{
+    if(setOn)
+    {
+        $("#waitWindow").slideDown();
+    }
+    else
+    {
+        $("#waitWindow").slideUp();
     }
 }
